986653e4c5fbaee4e645ba7fdb272438cb9677cd
d1s0l <d.solovyov214@gmail.com>
2026-08-31 19:13:45 +0300

    feat: add route protection and auth guard

--- src/app/router/AppRouter.tsx
+++ src/app/router/AppRouter.tsx
@@ -4,6 +4,9 @@ import HomePage from '../../pages/home/ui/HomePage';
 import LoginPage from '../../pages/login/ui/LoginPage';
 import RegisterPage from '../../pages/register/ui/RegisterPage';
 import ProfilePage from '../../pages/profile/ui/ProfilePage';
+import AuthGuard from './guards/AuthGuard';
+import CreateCardPage from '../../pages/create-card/ui/CreateCardPage';
+import FindPage from '../../pages/find/ui/Find';
 
 export function AppRouter() {
     return (
@@ -16,16 +19,28 @@ export function AppRouter() {
             <Route
                 path="/login"
                 element={<LoginPage />}
-            />
+                />
 
             <Route
                 path="/register"
                 element={<RegisterPage />}
-            />
-            <Route
-                path='/profile'
-                element={<ProfilePage />}
-            />
+                />
+
+            <Route element={<AuthGuard />}>
+                <Route
+                    path='/profile'
+                    element={<ProfilePage />}
+                />
+                <Route
+                    path='/create-card'
+                    element={<CreateCardPage />}
+                />
+                <Route
+                    path='/find'
+                    element={<FindPage />}
+                />
+            </Route>
+
         </Routes>
     );
 }
